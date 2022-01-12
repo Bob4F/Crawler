ee13f83b65fa6b38ebdb6ddca48bef17763bd589
方雲锋 <13715166404@163.com>
2022-01-12 22:35:43 +0800

    feat: 增加条件语句练习

---
+++ condition.py
@@ -0,0 +1,30 @@
+# -*- coding:UTF-8 -*-
+#@Author fyf
+import random
+
+score = 77
+
+if score >= 90 :
+    print("A")
+elif score >= 75 :
+    print("B")
+else:
+    print("C")
+
+
+print("----------------")
+
+
+sex = 1
+if sex == 1 :
+    print("男性")
+elif sex == 0:
+    print("女性")
+print("----------------")
+
+x = random.randint(0, 10)
+print(x)
+
+
+y = random.random()
+print(y)
--- print.py
+++ print.py
@@ -1,11 +1,24 @@
-#-*- coding = utf-8 -*-
+# -*- coding:UTF-8 -*-
 #@Author fyf
 #@Software VS CODE
 import keyword
 
 a = 10
 
-print a
 
-print "关键字"
-print keyword.kwlist
+print("hi")
+print("今年 %d", a)
+
+# print "关键字"
+
+print(keyword.kwlist)
+
+print("http","baidu","com", sep=".")
+
+print(type(a))
+
+strType = "ABC"
+print(type(strType))
+
+inputStr = input("输入：")
+print(type(inputStr))
