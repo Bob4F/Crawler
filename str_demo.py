e85021e1471b3fc6799bef2d7182de6c53f4c7e1
方雲锋 <13715166404@163.com>
2022-01-14 14:37:23 +0800

    feat: 字符串练习

---
+++ str_demo.py
@@ -0,0 +1,36 @@
+# -*- coding:UTF-8 -*-
+#@Author Fyf
+word = "字符串"
+sentence = "这是一个橘"
+
+paragraph = """
+    这是一个段落
+    由多行组成
+
+"""
+
+print(word)
+print(sentence)
+print(paragraph)
+
+my_str = "I'm a dev"
+print(my_str)
+
+var1 = "I\'m a dev"
+print(var1)
+
+var2 = "I said \"Hello  World\""
+print(var2)
+
+var3 = "shenzhen"
+print(var3[3:10])
+print(var3[1:8:2])
+print(var3[4:])
+print(var3[:4])
+
+
+print(var3 + "你好")
+
+
+print("hello \t" + var3)
+print (r"hello \t" + var3)
