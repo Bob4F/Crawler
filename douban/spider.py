b323a298e1463bc7b0fdd8fcbf66011d394f6309
方雲锋 <13715166404@163.com>
2022-01-17 16:05:25 +0800

    feat: 处理需要添加的资源

---
+++ douban/spider.py
@@ -0,0 +1,17 @@
+#-*- codeing = utf-8 -*-
+#@Author: Fyf
+
+import sys
+from bs4 import BeautifulSoup
+import re
+import urllib
+import xlwt
+
+def main(): 
+    print("hello")
+
+
+
+if __name__ == "__main__":
+    # invoke method
+    main()
