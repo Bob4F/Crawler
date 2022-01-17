81f6f4519a22429966a43681417f971352b154b0
方雲锋 <13715166404@163.com>
2022-01-17 16:46:27 +0800

    feat:转存html文件

--- douban/UrllibTest.py
+++ douban/UrllibTest.py
@@ -3,13 +3,11 @@
 
 import urllib.request
 
-
+# 获取get请求
 response = urllib.request.urlopen("http://www.baidu.com")
 
 
-var1 = open(r"./百度.txt", "w")
+var1 = open(r"./百度.html", "w")
 
 var1.write(response.read().decode('utf-8'))
-
-
 var1.close
